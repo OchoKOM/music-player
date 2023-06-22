b2482cd240715399ccd218d24cee3f90307b843c
OchoKOM <118879243+OchoKOM@users.noreply.github.com>
2023-06-22 17:04:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 let play_list = document.querySelectorAll('.play-list source'),
-track_card = document.querySelector('.track'), 
-now_playing = document.querySelector('.now-playing'),
-track_img = document.querySelector('.track-img'),
-track_name = document.querySelector('.track-name'),
-track_artist = document.querySelector('.track-artist');
+    track_card = document.querySelector('.track'),
+    now_playing = document.querySelector('.now-playing'),
+    track_img = document.querySelector('.track-img'),
+    track_name = document.querySelector('.track-name'),
+    track_artist = document.querySelector('.track-artist');
 
 let playpause_btn = document.querySelector('.playpause-track'),
-next_btn = document.querySelector('.next-track'),
-prev_btn = document.querySelector('.prev-track');
+    next_btn = document.querySelector('.next-track'),
+    prev_btn = document.querySelector('.prev-track');
 
 let seek = document.querySelector('.seek-slider'),
-seek_slider = document.querySelector('.seek-slider .slider'),
-volume_slider = document.querySelector('.volume-slider'),
-curr_time = document.querySelector('.current-time'),
-total_duration = document.querySelector('.duration'),
-wave = document.getElementById('wave'),
-waves = document.querySelectorAll('#wave span'),
-randomIcon = document.querySelector('.random-track'),
-repeatIcon = document.querySelector('.repeat-track'),
-curr_track = document.createElement('audio');
+    seek_slider = document.querySelector('.seek-slider .slider'),
+    volume_slider = document.querySelector('.volume-slider'),
+    curr_time = document.querySelector('.current-time'),
+    total_duration = document.querySelector('.duration'),
+    wave = document.getElementById('wave'),
+    waves = document.querySelectorAll('#wave span'),
+    randomIcon = document.querySelector('.random-track'),
+    repeatIcon = document.querySelector('.repeat-track'),
+    curr_track = document.createElement('audio');
 
 let track_index = 0,
-isPlaying = false,
-isRandom = false,
-updateTimer,
-music_list = [];
+    isPlaying = false,
+    isRandom = false,
+    updateTimer,
+    music_list = [];
 
 const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
     minimumIntegerDigits: 2,
-  });
+});
 for (let i = 0; i < play_list.length; i++) {
     const track_info = play_list[i],
-    track_img = track_info.getAttribute('data-image'),
-    track_name = track_info.getAttribute('data-name'),
-    track_artist = track_info.getAttribute('data-artist'),
-    track_source = track_info.src;
+        track_img = track_info.getAttribute('data-image'),
+        track_name = track_info.getAttribute('data-name'),
+        track_artist = track_info.getAttribute('data-artist'),
+        track_source = track_info.src;
 
     let info = {
-        img : track_img,
-        name : track_name,
-        artist : track_artist,
-        music : track_source
+        img: track_img,
+        name: track_name,
+        artist: track_artist,
+        music: track_source
     }
     music_list.push(info)
 }
@@ -56,30 +56,31 @@ function formatDuration(time) {
     } else {
         return `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`;
     }
-  }
-  //! Fonction pour les ecrans tactiles 
-var init = function() {
+}
+//! Fonction pour les ecrans tactiles 
+//! Touchscreen slider function
+var init = function () {
     document.addEventListener('touchstart', handler, true);
     document.addEventListener('touchmove', handler, true);
     document.addEventListener('touchend', handler, true);
-    document.addEventListener('touchcancel', handler, true);        
-    };
-    var handler = function touch(event) {
-        var touch = event.changedTouches[0],
-            simulatedEvent = document.createEvent('MouseEvent');
-    
-        simulatedEvent.initMouseEvent(
-            { touchstart: 'mousedown', touchmove: 'mousemove', touchend: 'mouseup' } [event.type],
-            true, true, window, 1, 
-            touch.screenX, touch.screenY, touch.clientX, touch.clientY,
-            false, false, false, false, 0, null);
-    
-        touch.target.dispatchEvent(simulatedEvent);
-    };
+    document.addEventListener('touchcancel', handler, true);
+};
+var handler = function touch(event) {
+    var touch = event.changedTouches[0],
+        simulatedEvent = document.createEvent('MouseEvent');
+
+    simulatedEvent.initMouseEvent(
+        { touchstart: 'mousedown', touchmove: 'mousemove', touchend: 'mouseup' }[event.type],
+        true, true, window, 1,
+        touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+        false, false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+};
 
 loadTrack(track_index);
 
-function loadTrack(track_index){
+function loadTrack(track_index) {
     clearInterval(updateTimer);
     reset();
     curr_track.autoplay = true
@@ -91,19 +92,18 @@ function loadTrack(track_index){
     track_artist.textContent = music_list[track_index].artist;
     now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
 
-    curr_track.ontimeupdate = setUpdate
-
+    curr_track.addEventListener("play", playTrack)
+    curr_track.addEventListener("pause", pauseTrack)
+    curr_track.addEventListener("timeupdate", setUpdate)
     curr_track.addEventListener('ended', nextTrack);
     random_bg_color();
-
-    curr_track.onplay = playTrack();
 }
 
-function random_bg_color(){
+function random_bg_color() {
     let hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'];
 
-    function populate(a){
-        for(let i=0; i<6; i++){
+    function populate(a) {
+        for (let i = 0; i < 6; i++) {
             let x = Math.round(Math.random() * 14);
             let y = hex[x];
             a += y;
@@ -117,89 +117,78 @@ function random_bg_color(){
     let Color1 = populate('#');
     let Color2 = populate('#');
 
-
     var angle = 'to right';
-
     let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
     document.body.style.background = gradient;
 }
-function reset(){
-    curr_time.textContent = "00:00";
-    total_duration.textContent = "00:00";
-    seek_slider.style.setProperty("--t", '0')
+function reset() {
+    curr_track.currentTime = 0
+    seek_slider.style.setProperty("--t", '0');
 }
-
-function randomTrack(){
+function randomTrack() {
     isRandom ? pauseRandom() : playRandom();
 }
-function playRandom(){
+function playRandom() {
     isRandom = true;
     randomIcon.classList.remove('disabled');
 }
-function pauseRandom(){
+function pauseRandom() {
     isRandom = false;
     randomIcon.classList.add('disabled');
 }
-function repeatTrack(){
+function repeatTrack() {
     let current_index = track_index;
     loadTrack(current_index);
     playTrack();
 }
-function playpauseTrack(){
+function playpauseTrack() {
     isPlaying ? pauseTrack() : playTrack();
 }
-function playTrack(){
-    curr_track.play().then(function() { 
-       // Votre code ici ⛶ 
-     }).catch(function() { 
-       // Votre code ici ⛶ 
-     });;
+function playTrack() {
+    curr_track.play();
     isPlaying = true;
     track_img.querySelector('img').classList.add('rotate');
     wave.classList.add('loader');
     track_card.classList.remove("paused");
 }
-function pauseTrack(){
+function pauseTrack() {
     curr_track.pause();
     isPlaying = false;
     track_img.querySelector('img').classList.remove('rotate');
     wave.classList.remove('loader');
     track_card.classList.add("paused");
 }
-function nextTrack(){
-    if(track_index < music_list.length - 1 && isRandom === false){
+function nextTrack() {
+    if (track_index < music_list.length - 1 && isRandom === false) {
         track_index += 1;
-    }else if(track_index < music_list.length - 1 && isRandom === true){
+    } else if (track_index < music_list.length - 1 && isRandom === true) {
         let random_index = Number.parseInt(Math.random() * music_list.length);
         track_index = random_index;
-    }else{
+    } else {
         track_index = 0;
     }
     loadTrack(track_index);
     playTrack();
 }
-function prevTrack(){
-    if(track_index > 0){
+function prevTrack() {
+    if (track_index > 0) {
         track_index -= 1;
-    }else{
-        track_index = music_list.length -1;
+    } else {
+        track_index = music_list.length - 1;
     }
     loadTrack(track_index);
     playTrack();
 }
-function seekTo(e){
+function seekTo(e) {
     let seekto = e.offsetX / seek.clientWidth;
     seek_slider.style.setProperty("--t", (seekto * 100) + '%')
-    curr_track.currentTime = Math.floor(seekto * curr_track.duration);
+    seek_pos = Math.floor(seekto * curr_track.duration);
+    curr_track.currentTime = seek_pos;
 }
-function setVolume(){
-    curr_track.volume = volume_slider.value / 100;
-}
-function setUpdate(){
+function setUpdate() {
     let seekPosition = 0;
-    if(!isNaN(curr_track.duration)){
+    if (!isNaN(curr_track.duration)) {
         seekPosition = (curr_track.currentTime / curr_track.duration) * 100;
-        
         seek_slider.style.setProperty("--t", seekPosition + '%')
 
         let current = formatDuration(curr_track.currentTime);
@@ -210,7 +199,7 @@ function setUpdate(){
     }
 }
 function autoScroll(el) {
-let scrollPx;
+    let scrollPx;
     setInterval(() => {
         if (el.scrollWidth - el.scrollLeft === el.clientWidth) {
             scrollPx = -1;
@@ -218,18 +207,20 @@ let scrollPx;
         if (el.scrollLeft === 0) {
             scrollPx = 1;
         }
-        el.scrollBy(scrollPx,0);
+        el.scrollBy(scrollPx, 0);
     }, 90);
 }
 autoScroll(track_name);
 autoScroll(track_artist);
 
-volume_slider.onmousemove = setVolume;
-seek.addEventListener("touchstart",init,true);
+volume_slider.onmousemove = () => {
+    curr_track.volume = volume_slider.value / 100;
+}
+seek.addEventListener("touchstart", init, true);
 seek.addEventListener("click", seekTo);
-seek.addEventListener("mousedown",()=>{
+seek.addEventListener("mousedown", () => {
     seek.addEventListener("mousemove", seekTo)
-    document.addEventListener("mouseup",()=>{
+    document.addEventListener("mouseup", () => {
         seek.removeEventListener("mousemove", seekTo)
     })
 })
@@ -237,3 +228,4 @@ next_btn.onclick = nextTrack;
 prev_btn.onclick = prevTrack;
 playpause_btn.onclick = playpauseTrack;
 randomIcon.onclick = randomTrack;
+repeatIcon.onclick = reset;
